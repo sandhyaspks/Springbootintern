f46f46cf6790ee89d4bf08eb73878d003463080c
sandhyaspks <sandhya.s2023ece@sece.ac.in>
2025-07-07 18:52:43 +0530

    first commit

---
+++ src/main/java/org/example/Prof.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Prof {
+    public Prof() {
+        System.out.println("This is prof");
+    }
+    public void proff(){
+        System.out.println("this is method of prof");
+    }
+}
